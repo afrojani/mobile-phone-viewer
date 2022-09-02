ee0ca7acb56125b0a33b44942a2785881b856e58
afrojani <sultana.ani2019@gmail.com>
2022-09-02 02:24:30 -0500

    initial structure

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const loadPhones = async () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data);
+
+}
+
+const displayPhones = phones => {
+    // console.log(phones);
+    phones.forEach(phone => {
+        console.log(phone);
+        const phoneContainer = document.getElementById('phone-container');
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                to additional content. This content is a little bit longer.</p>
+        </div>
+    </div>
+        `;
+        phoneContainer.appendChild(phoneDiv);
+
+    });
+}
+
+loadPhones();
